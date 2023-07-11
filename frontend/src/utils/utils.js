cc4e5972834935eeafa3f602d7a4005593a10386
Sergey Kornilov <amadeusdekond@yandex.ru>
2023-07-11 17:51:19 +0300

    подготовка к деплою

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,6 +1,6 @@
 export const apiConfig = {
-//    url: "http://localhost:3000",
-    url: "https://api.mesto.glen120.nomoredomains.work",
+    url: "http://localhost:3000",
+//    url: "https://api.mesto.glen120.nomoredomains.work",
     headers: {
         "content-type": "application/json",
     }
